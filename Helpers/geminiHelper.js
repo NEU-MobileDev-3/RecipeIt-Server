8f3c93c172e583f2b5f699afc9aa0441ecd26d9b
laolarou <luolinrui726@outlook.com>
2025-04-13 15:33:49 -0700

    Update geminiHelper.js

--- Helpers/geminiHelper.js
+++ Helpers/geminiHelper.js
@@ -9,12 +9,23 @@ const GEMINI_DEFAULT_MODEL = "gemini-2.0-flash";
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
-const DEFAULT_PROMPT_HEADER =
+const DEFAULT_PROMPT_HEADER_IMAGE =
     "You are a helpful assistant which will help user to generate some potential food recipes based on the image content: \n" +
     "Please provide a brief description of the image content. \n" +
     "For example, you can say 'This is a picture of a table with tomatoes, pepperoni and cheese on it'. \n" +
     "If you can not recognize the image, please say 'I can not recognize the image'. \n" +
     "You can assume that the image only contains food ingredients. \n" +
+    "For the recipe generation you need to follow the following rules: \n" +
+    "1. The title of the recipe \n" +
+    "2. The ingredients of the recipe \n" +
+    "3. The steps to prepare the recipe \n" +
+    "4. The time to prepare the recipe \n" +
+    "5. The number of servings \n" +
+    "Please use newline to separate the different parts of the recipe. \n" +
+    "Please also add a new line to separate the different recipes.";
+
+const DEFAULT_PROMPT_HEADER_CHAT =
+    "You are a helpful assistant which will help user to generate some potential food recipes based on the user's input: \n" +
     "For the recipe generation you need to follow the following rules: \n" +
     "1. The title of the recipe \n" +
     "2. The ingredients of the recipe \n" +
@@ -33,7 +44,7 @@ async function createImagePart(base64Str) {
 
     return [
         createUserContent([
-            DEFAULT_PROMPT_HEADER,
+            DEFAULT_PROMPT_HEADER_IMAGE,
             createPartFromUri(image.uri, image.mimeType),
         ]),
     ];
@@ -53,7 +64,7 @@ export async function generateRecipeByChat(query) {
         model: GEMINI_DEFAULT_MODEL,
         contents: [
             createUserContent([
-                DEFAULT_PROMPT_HEADER,
+                DEFAULT_PROMPT_HEADER_CHAT,
                 query,
             ]),
         ],
